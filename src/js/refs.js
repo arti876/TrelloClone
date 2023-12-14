2ee02968169f88ea04eb15bb1ec618d80d824b8a
arti876 <e.artem.v@gmail.com>
2023-12-14 15:42:47 +0300

    commit

--- src/js/refs.js
+++ src/js/refs.js
@@ -24,3 +24,32 @@ return (
     ))}
   </div>
 );
+
+function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+      const fetchData = async () => {
+          const result = await axios.get (
+              "https://jsonplaceholder.typicode.com/posts",
+          );
+
+          setPosts(result.data);
+      };
+      
+      fetchData();
+  }, []);
+      return (
+             <div className='container'>
+                  <div className='user-cards'>
+                      {posts && 
+                          posts.map(post => (
+                              <div className='user-card'>
+                                  <p>{post.title}</p>
+                                  <p>{post.body}</p>
+                              </div>
+                          ))}
+                  </div>
+              </div>
+ )
+}
